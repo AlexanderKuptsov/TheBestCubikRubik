13bdf9c6bbfc28e939005338f2a9e95e76963e1f
AlexanderKuptsov <shon55398@gmail.com>
2017-03-05 18:53:36 +0300

    some fixes

--- src/Cube/AllSides.java
+++ src/Cube/AllSides.java
@@ -24,7 +24,7 @@ public class AllSides {
         colors[3] = Color.YELLOW;
         colors[4] = Color.Pink;
         colors[5] = Color.GREEN;
-        boolean check = false;
+        boolean check;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
@@ -86,187 +86,193 @@ public class AllSides {
         return str.toString();
     }
 
-    private void rotate(int side, int direction) { // метод поворота любой стороны кубика напрво или налево
-        Color[][][] help = new Color[6][3][3];
+    private void helpMemory(Color[][][] help) {
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 3; j++) {
                 System.arraycopy(memory[i][j], 0, help[i][j], 0, 3);
             }
         }
+    }
 
-        if (direction == 1) { //поворот направо
-            for (int i = 0; i <= 2; i++) {
-                for (int k = 2; k >= 0; k--) {
-                    memory[side][i][2 - k] = help[side][k][i];
-                }
-            }
+    private void rotateRight(int side) { // метод поворота любой стороны кубика напрво
+        Color[][][] help = new Color[6][3][3];
+        helpMemory(help);
 
-            if (side == 4) { //поворот верхней грани
-                for (int i = 0; i <= 3; i++) {
-                    int k;
-                    if (i == 0) k = 3;
-                    else k = i - 1;
-                    memory[i][0][0] = help[k][0][0];
-                    memory[i][0][1] = help[k][0][1];
-                    memory[i][0][2] = help[k][0][2];
-                }
-            }
-            if (side == 5) { //поворот нижней грани
-                for (int i = 0; i <= 3; i++) {
-                    int k;
-                    if (i == 0) k = 3;
-                    else k = i - 1;
-                    memory[i][2][0] = help[k][2][0];
-                    memory[i][2][1] = help[k][2][1];
-                    memory[i][2][2] = help[k][2][2];
-                }
-            }
-            if (side == 1) { //поворот боковой грани (правая)
-                for (int i = 0; i <= 2; i += 2) {
-                    int k, j = 0;
-                    if (i == 0) k = 5;
-                    else {
-                        k = 4;
-                        j = 2;
-                    }
-                    memory[i][0][2] = help[k][j][2];
-                    memory[i][1][2] = help[k][1][2];
-                    memory[i][2][2] = help[k][2 - j][2];
-                    memory[k][0][2] = help[2 - i][2 - j][2];
-                    memory[k][1][2] = help[2 - i][1][2];
-                    memory[k][2][2] = help[2 - i][j][2];
-                }
-            }
-            if (side == 2) { //поворот боковой грани (задняя)
-                for (int i = 1; i <= 3; i += 2) {
-                    int k, j = 0;
-                    if (i == 1) k = 5;
-                    else {
-                        k = 4;
-                        j = 2;
-                    }
-                    memory[i][0][2 - j] = help[k][2 - j][2];
-                    memory[i][1][2 - j] = help[k][2 - j][1];
-                    memory[i][2][2 - j] = help[k][2 - j][0];
-                    memory[k][2 - j][0] = help[4 - i][0][j];
-                    memory[k][2 - j][1] = help[4 - i][1][j];
-                    memory[k][2 - j][2] = help[4 - i][2][j];
-                }
-            }
-            if (side == 3) { //поворот боковой грани (левая)
-                for (int i = 0; i <= 2; i += 2) {
-                    int k, j = 0;
-                    if (i == 0) k = 4;
-                    else {
-                        k = 5;
-                        j = 2;
-                    }
-                    memory[i][0][j] = help[k][j][0];
-                    memory[i][1][j] = help[k][1][0];
-                    memory[i][2][j] = help[k][2 - j][0];
-                    memory[k][0][0] = help[2 - i][2 - j][2 - j];
-                    memory[k][1][0] = help[2 - i][1][2 - j];
-                    memory[k][2][0] = help[2 - i][j][2 - j];
-                }
-            }
-            if (side == 0) { //поворот боковой грани (фронтальная)
-                memory[4][2][0] = help[3][2][2];
-                memory[4][2][1] = help[3][1][2];
-                memory[4][2][2] = help[3][0][2];
-                memory[1][0][0] = help[4][2][0];
-                memory[1][1][0] = help[4][2][1];
-                memory[1][2][0] = help[4][2][2];
-                memory[5][0][0] = help[1][2][0];
-                memory[5][0][1] = help[1][1][0];
-                memory[5][0][2] = help[1][0][0];
-                memory[3][0][2] = help[5][0][0];
-                memory[3][1][2] = help[5][0][1];
-                memory[3][2][2] = help[5][0][2];
+        for (int i = 0; i <= 2; i++) {
+            for (int k = 2; k >= 0; k--) {
+                memory[side][i][2 - k] = help[side][k][i];
             }
         }
 
-        if (direction == 2) { //поворот налево
-            for (int i = 0; i <= 2; i++) {
-                for (int k = 2; k >= 0; k--) {
-                    memory[side][2 - k][i] = help[side][i][k];
+        if (side == 4) { //поворот верхней грани
+            for (int i = 0; i <= 3; i++) {
+                int k;
+                if (i == 0) k = 3;
+                else k = i - 1;
+                memory[i][0][0] = help[k][0][0];
+                memory[i][0][1] = help[k][0][1];
+                memory[i][0][2] = help[k][0][2];
+            }
+        }
+        if (side == 5) { //поворот нижней грани
+            for (int i = 0; i <= 3; i++) {
+                int k;
+                if (i == 0) k = 3;
+                else k = i - 1;
+                memory[i][2][0] = help[k][2][0];
+                memory[i][2][1] = help[k][2][1];
+                memory[i][2][2] = help[k][2][2];
+            }
+        }
+        if (side == 1) { //поворот боковой грани (правая)
+            for (int i = 0; i <= 2; i += 2) {
+                int k, j = 0;
+                if (i == 0) k = 5;
+                else {
+                    k = 4;
+                    j = 2;
                 }
+                memory[i][0][2] = help[k][j][2];
+                memory[i][1][2] = help[k][1][2];
+                memory[i][2][2] = help[k][2 - j][2];
+                memory[k][0][2] = help[2 - i][2 - j][2];
+                memory[k][1][2] = help[2 - i][1][2];
+                memory[k][2][2] = help[2 - i][j][2];
             }
-            if (side == 4) { //поворот верхней грани
-                for (int i = 0; i <= 3; i++) {
-                    int k;
-                    if (i == 3) k = 0;
-                    else k = i + 1;
-                    memory[i][0][0] = help[k][0][0];
-                    memory[i][0][1] = help[k][0][1];
-                    memory[i][0][2] = help[k][0][2];
+        }
+        if (side == 2) { //поворот боковой грани (задняя)
+            for (int i = 1; i <= 3; i += 2) {
+                int k, j = 0;
+                if (i == 1) k = 5;
+                else {
+                    k = 4;
+                    j = 2;
                 }
+                memory[i][0][2 - j] = help[k][2 - j][2];
+                memory[i][1][2 - j] = help[k][2 - j][1];
+                memory[i][2][2 - j] = help[k][2 - j][0];
+                memory[k][2 - j][0] = help[4 - i][0][j];
+                memory[k][2 - j][1] = help[4 - i][1][j];
+                memory[k][2 - j][2] = help[4 - i][2][j];
             }
-            if (side == 5) { //поворот нижней грани грани
-                for (int i = 0; i <= 4; i++) {
-                    int k;
-                    if (i == 3) k = 0;
-                    else k = i + 1;
-                    memory[i][2][0] = help[k][2][0];
-                    memory[i][2][1] = help[k][2][1];
-                    memory[i][2][2] = help[k][2][2];
+        }
+        if (side == 3) { //поворот боковой грани (левая)
+            for (int i = 0; i <= 2; i += 2) {
+                int k, j = 0;
+                if (i == 0) k = 4;
+                else {
+                    k = 5;
+                    j = 2;
                 }
+                memory[i][0][j] = help[k][j][0];
+                memory[i][1][j] = help[k][1][0];
+                memory[i][2][j] = help[k][2 - j][0];
+                memory[k][0][0] = help[2 - i][2 - j][2 - j];
+                memory[k][1][0] = help[2 - i][1][2 - j];
+                memory[k][2][0] = help[2 - i][j][2 - j];
             }
-            if (side == 0) { //поворот боковой грани (фронтальная)
-                memory[4][2][0] = help[1][0][0];
-                memory[4][2][1] = help[1][1][0];
-                memory[4][2][2] = help[1][2][0];
-                memory[1][0][0] = help[5][0][2];
-                memory[1][1][0] = help[5][0][1];
-                memory[1][2][0] = help[5][0][0];
-                memory[5][0][0] = help[3][0][2];
-                memory[5][0][1] = help[3][1][2];
-                memory[5][0][2] = help[3][2][2];
-                memory[3][0][2] = help[4][2][2];
-                memory[3][1][2] = help[4][2][1];
-                memory[3][2][2] = help[4][2][0];
+        }
+        if (side == 0) { //поворот боковой грани (фронтальная)
+            memory[4][2][0] = help[3][2][2];
+            memory[4][2][1] = help[3][1][2];
+            memory[4][2][2] = help[3][0][2];
+            memory[1][0][0] = help[4][2][0];
+            memory[1][1][0] = help[4][2][1];
+            memory[1][2][0] = help[4][2][2];
+            memory[5][0][0] = help[1][2][0];
+            memory[5][0][1] = help[1][1][0];
+            memory[5][0][2] = help[1][0][0];
+            memory[3][0][2] = help[5][0][0];
+            memory[3][1][2] = help[5][0][1];
+            memory[3][2][2] = help[5][0][2];
+        }
+
+
+    }
+
+    private void rotateLeft(int side) { // метод поворота любой стороны кубика налево
+        Color[][][] help = new Color[6][3][3];
+        helpMemory(help);
+        for (int i = 0; i <= 2; i++) {
+            for (int k = 2; k >= 0; k--) {
+                memory[side][2 - k][i] = help[side][i][k];
             }
-            if (side == 1) { //поворот боковой грани (правая)
-                memory[0][0][2] = help[4][0][2];
-                memory[0][1][2] = help[4][1][2];
-                memory[0][2][2] = help[4][2][2];
-                memory[4][0][2] = help[2][2][0];
-                memory[4][1][2] = help[2][1][0];
-                memory[4][2][2] = help[2][0][0];
-                memory[2][0][0] = help[5][2][2];
-                memory[2][1][0] = help[5][1][2];
-                memory[2][2][0] = help[5][0][2];
-                memory[5][0][2] = help[0][0][2];
-                memory[5][1][2] = help[0][1][2];
-                memory[5][2][2] = help[0][2][2];
+        }
+        if (side == 4) { //поворот верхней грани
+            for (int i = 0; i <= 3; i++) {
+                int k;
+                if (i == 3) k = 0;
+                else k = i + 1;
+                memory[i][0][0] = help[k][0][0];
+                memory[i][0][1] = help[k][0][1];
+                memory[i][0][2] = help[k][0][2];
             }
-            if (side == 2) { //поворот боковой грани (задняя)
-                memory[1][0][2] = help[4][0][0];
-                memory[1][1][2] = help[4][0][1];
-                memory[1][2][2] = help[4][0][2];
-                memory[4][0][0] = help[3][2][0];
-                memory[4][0][1] = help[3][1][0];
-                memory[4][0][2] = help[3][0][0];
-                memory[3][0][0] = help[5][2][0];
-                memory[3][1][0] = help[5][2][1];
-                memory[3][2][0] = help[5][2][2];
-                memory[5][2][2] = help[1][0][2];
-                memory[5][2][1] = help[1][1][2];
-                memory[5][2][0] = help[1][2][2];
+        }
+        if (side == 5) { //поворот нижней грани грани
+            for (int i = 0; i <= 4; i++) {
+                int k;
+                if (i == 3) k = 0;
+                else k = i + 1;
+                memory[i][2][0] = help[k][2][0];
+                memory[i][2][1] = help[k][2][1];
+                memory[i][2][2] = help[k][2][2];
             }
-            if (side == 3) { //поворот боковой грани (левая)
-                memory[0][0][0] = help[5][0][0];
-                memory[0][1][0] = help[5][1][0];
-                memory[0][2][0] = help[5][2][0];
-                memory[4][0][0] = help[0][0][0];
-                memory[4][1][0] = help[0][1][0];
-                memory[4][2][0] = help[0][2][0];
-                memory[2][0][2] = help[4][2][0];
-                memory[2][1][2] = help[4][1][0];
-                memory[2][2][2] = help[4][0][0];
-                memory[5][0][0] = help[2][2][2];
-                memory[5][1][0] = help[2][1][2];
-                memory[5][2][0] = help[2][0][2];
-            }
+        }
+        if (side == 0) { //поворот боковой грани (фронтальная)
+            memory[4][2][0] = help[1][0][0];
+            memory[4][2][1] = help[1][1][0];
+            memory[4][2][2] = help[1][2][0];
+            memory[1][0][0] = help[5][0][2];
+            memory[1][1][0] = help[5][0][1];
+            memory[1][2][0] = help[5][0][0];
+            memory[5][0][0] = help[3][0][2];
+            memory[5][0][1] = help[3][1][2];
+            memory[5][0][2] = help[3][2][2];
+            memory[3][0][2] = help[4][2][2];
+            memory[3][1][2] = help[4][2][1];
+            memory[3][2][2] = help[4][2][0];
+        }
+        if (side == 1) { //поворот боковой грани (правая)
+            memory[0][0][2] = help[4][0][2];
+            memory[0][1][2] = help[4][1][2];
+            memory[0][2][2] = help[4][2][2];
+            memory[4][0][2] = help[2][2][0];
+            memory[4][1][2] = help[2][1][0];
+            memory[4][2][2] = help[2][0][0];
+            memory[2][0][0] = help[5][2][2];
+            memory[2][1][0] = help[5][1][2];
+            memory[2][2][0] = help[5][0][2];
+            memory[5][0][2] = help[0][0][2];
+            memory[5][1][2] = help[0][1][2];
+            memory[5][2][2] = help[0][2][2];
+        }
+        if (side == 2) { //поворот боковой грани (задняя)
+            memory[1][0][2] = help[4][0][0];
+            memory[1][1][2] = help[4][0][1];
+            memory[1][2][2] = help[4][0][2];
+            memory[4][0][0] = help[3][2][0];
+            memory[4][0][1] = help[3][1][0];
+            memory[4][0][2] = help[3][0][0];
+            memory[3][0][0] = help[5][2][0];
+            memory[3][1][0] = help[5][2][1];
+            memory[3][2][0] = help[5][2][2];
+            memory[5][2][2] = help[1][0][2];
+            memory[5][2][1] = help[1][1][2];
+            memory[5][2][0] = help[1][2][2];
+        }
+        if (side == 3) { //поворот боковой грани (левая)
+            memory[0][0][0] = help[5][0][0];
+            memory[0][1][0] = help[5][1][0];
+            memory[0][2][0] = help[5][2][0];
+            memory[4][0][0] = help[0][0][0];
+            memory[4][1][0] = help[0][1][0];
+            memory[4][2][0] = help[0][2][0];
+            memory[2][0][2] = help[4][2][0];
+            memory[2][1][2] = help[4][1][0];
+            memory[2][2][2] = help[4][0][0];
+            memory[5][0][0] = help[2][2][2];
+            memory[5][1][0] = help[2][1][2];
+            memory[5][2][0] = help[2][0][2];
         }
     }
 
@@ -304,8 +310,9 @@ public class AllSides {
             System.out.println("Choose direction right(1)/left(2)");
             Scanner input = new Scanner((System.in));
             direction = input.nextInt();
-            if ((side >= 1 && side <= 6) && (direction == 1 || direction == 2)) {
-                result.rotate(side - 1, direction);
+            if ((side >= 1 && side <= 6) && (direction == 1 || direction == 2)) { //проверка введенных данных
+                if (direction == 1) result.rotateRight(side - 1);
+                else result.rotateLeft(side - 1);
                 System.out.println(result.toString());
             } else {
                 System.out.print("Write correct information!");
